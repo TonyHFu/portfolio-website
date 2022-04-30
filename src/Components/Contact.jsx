1b603e89adf0cd57684265b948b1466fa2a783fa
tofutigerz <hfu3@ualberta.ca>
2022-04-30 11:05:15 -0600

    implements state handling

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,15 +1,67 @@
+import { useState } from "react";
+import axios from "axios";
 import "./styles/Contact.scss";
 export default function Contact(props) {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [message, setMessage] = useState("");
+	const [sent, setSent] = useState(false);
+
+	const handleChangeName = e => {
+		setName(e.target.value);
+	};
+
+	const handleChangeEmail = e => {
+		setEmail(e.target.value);
+	};
+
+	const handleChangeMessage = e => {
+		setMessage(e.target.value);
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		axios
+			.post("http://localhost:8888/email-form.php", {
+				name,
+				email,
+				message,
+			})
+			.then(() => {
+				console.log("successfully sent");
+				setName("");
+				setEmail("");
+				setMessage("");
+				setSent(true);
+			})
+			.catch(err => console.log(err));
+	};
+
 	return (
 		<div id="contact">
 			<div id="contact-link"></div>
-			<form id="contact-form">
+			<form id="contact-form" onSubmit={handleSubmit}>
 				<label for="name">Name</label>
-				<input type="text" name="name"></input>
+				<input
+					type="text"
+					name="name"
+					onChange={handleChangeName}
+					value={name}
+				></input>
 				<label for="email">Email</label>
-				<input type="email" name="email"></input>
+				<input
+					type="email"
+					name="email"
+					onChange={handleChangeEmail}
+					value={email}
+				></input>
 				<label for="message">Message</label>
-				<textarea type="text" name="message"></textarea>
+				<textarea
+					type="text"
+					name="message"
+					onChange={handleChangeMessage}
+					value={message}
+				></textarea>
 				<input type="submit"></input>
 			</form>
 		</div>
