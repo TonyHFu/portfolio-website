3f8b90bcaae0d06c2ae1e4d8e3d870b9099cf74f
tofutigerz <hfu3@ualberta.ca>
2022-04-30 10:00:51 -0600

    makes leftnav buttons work

--- src/Components/Leftnav.jsx
+++ src/Components/Leftnav.jsx
@@ -3,8 +3,12 @@ import "./styles/Leftnav.scss";
 export default function Leftnav(props) {
 	return (
 		<div id="left-nav">
-			<i class="fa-brands fa-github"></i>
-			<i class="fa-brands fa-linkedin-in"></i>
+			<a href="https://github.com/TonyHFu">
+				<i class="fa-brands fa-github"></i>
+			</a>
+			<a href="https://www.linkedin.com/in/tony-fu-847b5878/">
+				<i class="fa-brands fa-linkedin-in"></i>
+			</a>
 		</div>
 	);
 }
