867e15ac3b7c3e51192615100d99f91dddaa5902
tofutigerz <hfu3@ualberta.ca>
2022-05-03 12:31:30 -0600

    implements contact form with emailjs

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import axios from "axios";
 import "./styles/Contact.scss";
+import emailjs from "@emailjs/browser";
+
 export default function Contact(props) {
 	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
 	const [message, setMessage] = useState("");
 	const [sent, setSent] = useState(false);
+
+	const form = useRef();
 
 	const handleChangeName = e => {
 		setName(e.target.value);
@@ -21,26 +25,43 @@ export default function Contact(props) {
 
 	const handleSubmit = e => {
 		e.preventDefault();
-		axios
-			.post("http://localhost:8888/email-form.php", {
-				name,
-				email,
-				message,
-			})
-			.then(() => {
-				console.log("successfully sent");
-				setName("");
-				setEmail("");
-				setMessage("");
-				setSent(true);
-			})
-			.catch(err => console.log(err));
+		// axios
+		// 	.post("http://localhost:8888/email-form.php", {
+		// 		name,
+		// 		email,
+		// 		message,
+		// 	})
+		// 	.then(() => {
+		// 		console.log("email successfully sent");
+		// 		setName("");
+		// 		setEmail("");
+		// 		setMessage("");
+		// 		setSent(true);
+		// 	})
+		// 	.catch(err => console.log(err));
+
+		emailjs
+			.sendForm(
+				"service_cki031g",
+				"template_h5wl3nt",
+				form.current,
+				"20voThGYjy8Qli-bh"
+			)
+			.then(
+				result => {
+					console.log(result.text);
+					setSent(true);
+				},
+				error => {
+					console.log(error.text);
+				}
+			);
 	};
 
 	return (
 		<div id="contact">
 			<div id="contact-link"></div>
-			<form id="contact-form" onSubmit={handleSubmit}>
+			<form id="contact-form" ref={form} onSubmit={handleSubmit}>
 				<label for="name">Name</label>
 				<input
 					type="text"
